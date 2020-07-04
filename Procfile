650d29cea449d4a4f876e88fd5da0e2e130b6279
blacktyger <blacktyg3r@gmail.com>
2020-07-04 11:05:36 +0100

    procfile updated, django-heroku installed

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tickerv4.wsgi
--- tickerv4/settings.py
+++ tickerv4/settings.py
@@ -1,7 +1,10 @@
 import os
 from unipath import Path
+import django_heroku
 
 from .keys import *
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
